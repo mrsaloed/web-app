6f6ab276aa92653619a0b5032b3c0b02c179fd65
Mikhail Leonov <mikhail.leonov62@gmail.com>
2022-10-15 01:27:27 +0300

    Initial commit

---
+++ src/test/java/net/ddns/mrsaloed/webapp/WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ddns.mrsaloed.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
