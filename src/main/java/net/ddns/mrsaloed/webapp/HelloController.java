870599c28577b81d8cc67a2cd5a0199b98e1656f
Mikhail Leonov <mikhail.leonov62@gmail.com>
2022-10-28 11:16:34 +0300

    Attempt to redirect to another App

--- src/main/java/net/ddns/mrsaloed/webapp/HelloController.java
+++ src/main/java/net/ddns/mrsaloed/webapp/HelloController.java
@@ -1,13 +1,33 @@
 package net.ddns.mrsaloed.webapp;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 public class HelloController {
+    private static final String REDIRECT = "192.168.0.168";
+
     @GetMapping("/hello")
     public String hello(@RequestParam(required = false) String name) {
         return "Hello, " + name;
+    }
+    @RequestMapping(value = "/redirect", method = RequestMethod.GET)
+    public void method(HttpServletResponse response) {
+        try {
+            response.sendRedirect(REDIRECT);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @RequestMapping(value = "/forward", method = RequestMethod.GET)
+    public RedirectView myMethod (RedirectAttributes attributes) {
+        attributes.addFlashAttribute("flashAttribute", "forward");
+        attributes.addAttribute("attribute", "forward");
+        return new RedirectView("192.168.0.136");
     }
 }
